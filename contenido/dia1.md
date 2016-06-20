cdb00392978edbf45a9192e39020b34b72a961c6
ferflores <ferflores@gmail.com>
2016-06-19 19:01:00 -0500

    Day 1 content

---
+++ contenido/dia1.md
@@ -0,0 +1,23 @@
+# Frontend Development Semana 3 Día 1 (3 horas)
+
+### -Intro (15 min)
+
+* Objetivo de la Semana
+
+### - Javascript objetos (45 min)
+
+* Definición
+* this
+* prototype
+* Herencia
+
+### - Javascript scopes (1 hora)
+
+* Definición
+* Ejemplos
+* ¿Por qué son importantes los scopes?
+* Crear scopes
+* Closures
+
+
+# - Ejercicio de scopes y closures dibujando en canvas (1 hora)
---
+++ ejercicios/dia1/js/index.js
@@ -0,0 +1,57 @@
+var canvas = document.getElementById('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var context = canvas.getContext('2d');
+var figures = [];
+
+function clearCanvas(){
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawAllFigures(){
+  clearCanvas();
+  for (var i = 0; i < figures.length; i++) {
+    figures[i].move();
+    figures[i].draw();
+  }
+}
+
+function Square(posX, posY, width, height, velX, velY) {
+
+  this.draw = function(){
+    context.fillStyle = "red";
+    context.fillRect(posX, posY, width, height);
+  }
+
+  this.move = function() {
+    this.validations();
+    posX += velX;
+    posY += velY;
+  }
+
+  this.validations = function(){
+    if(posX > canvas.width - width || posX < 0){
+      velX = -velX;
+    }
+
+    if(posY > canvas.height - height || posY < 0){
+      velY = -velY;
+    }
+
+  }
+}
+
+function animate(){
+  drawAllFigures();
+  setTimeout(animate, 1);
+}
+
+animate();
+
+for (var i = 0; i < 100; i++) {
+  var randomX = Math.random() * 800;
+  var randomY = Math.random() * 600;
+  var randomSize = Math.random() * 100;
+
+  figures.push(new Square(randomX, randomY, randomSize, randomSize, 1, 1));
+}
